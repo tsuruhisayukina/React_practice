495238ecde20ae69b85a72257b00d8e1c6cafd55
tsuruhisayukina <tsuruhisayukina.0110@icloud.com>
2021-03-28 19:32:07 +0900

    ~p138

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,9 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "./layout.css"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
 export default ({ children }) => {
     return(
